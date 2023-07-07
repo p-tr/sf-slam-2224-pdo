f5221346636e33c977203d5f57691d6c60a1a975
Pierre Tellier-Robert <hi@p-tr.space>
2023-07-07 13:31:23 +0200

    Gestion des messages

--- lib/session.php
+++ lib/session.php
@@ -3,50 +3,50 @@
 //  Ce fichier contient la classe Session permettant de
 // gérer le conteneur de session de PHP.
 
+require('message.php');
+
 class Session {
+    const CURRENT_USER = 'user';
+    const MESSAGES = 'messages';
+
     public static function open() {
         session_start();
     }
 
     public static function login($user) {
-        $_SESSION['user'] = $user;
+        self::set(self::CURRENT_USER, $user);
     }
 
     public static function logout() {
-        unset($_SESSION['user']);
+        self::flush(self::CURRENT_USER);
     }
 
     public static function getUser() {
-        if(!isset($_SESSION['user'])) {
-            $_SESSION['user'] = null;
+        return self::get(self::CURRENT_USER);
+    }
+
+    public static function addMessage(Message $message) {
+        if(!is_array($_SESSION[self::MESSAGES])) {
+            $_SESSION[self::MESSAGES] = [];
         }
 
-        $user = $_SESSION['user'];
-        return $user;
+        $_SESSION[self::MESSAGES][] = $message;
     }
 
-    public static function setErrorMessage($message = '') {
-        $_SESSION['error_message'] = $message;
+    public static function getMessages(?Type $type = null) {
+        $messages = self::get(self::MESSAGES);
+
+        if(!is_null($type)) {
+            $messages = array_filter($messages, function($m) use($type) {
+                return $m->getType() == $type;
+            });
+        }
+
+        return $messages;
     }
 
-    public static function getErrorMessage() {
-        return isset($_SESSION['error_message']) ? $_SESSION['error_message'] : null;
-    }
-
-    public static function flushErrorMessage() {
-        unset($_SESSION['error_message']);
-    }
-
-    public static function getErrors() {
-        return isset($_SESSION['errors']) ? $_SESSION['errors'] : null;
-    }
-
-    public static function setErrors($errors) {
-        $_SESSION['errors'] = $errors;
-    }
-
-    public static function flushErrors() {
-        unset($_SESSION['errors']);
+    public static function flushMessages() {
+        self::flush(self::MESSAGES);
     }
 
     public static function get($key) {
